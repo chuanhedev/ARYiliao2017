cc349d2457430eb2af3a404e2ccde31b77b09ec3
cx <chixuchixu@hotmail.com>
2017-11-28 12:52:37 +0800

    init

---
+++ Assets/Scripts/FenziController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenziController : MonoBehaviour {
+
+	public GameObject[] labels;
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < labels.Length; i++) {
+			labels [i].transform.rotation = Quaternion.Euler(90, 0, 0);
+			labels [i].transform.Rotate (0, 180, 0);
+		}
+	}
+}
